fd74656dd019710816015aca7876044f4fcd4065
minghao <minghao1023@qq.com>
2021-03-16 19:11:23 +0800

    upload preprocess which will be finished in level2

--- src/main/java/preprocess/preprocess.java
+++ src/main/java/preprocess/preprocess.java
@@ -1,4 +1,13 @@
 package preprocess;
 
 public class preprocess {
+    private String text;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    /**
+     * 分词，去关键字，去停用词，词性变化，复合拆分
+     */
 }
---
+++ src/test/PreProcess/test/PreProcessTest.java
@@ -0,0 +1,10 @@
+package PreProcess.test;
+
+import org.junit.Test;
+
+public class PreProcessTest {
+    @Test
+    public void preprocessTest(){
+
+    }
+}
