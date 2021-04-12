93ea57663faff43e400aa7b3c3d5014fa30695cd
刘子良 <181850102@smail.nju.edu.cn>
2021-04-13 01:03:45 +0800

    preprocess update

---
+++ src/main/java/PreProcess/ReadFromFile.java
@@ -0,0 +1,205 @@
+package PreProcess;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFromFile {
+
+    static List<String> keyWordsList = new ArrayList<String>();
+    static List<String> stopWordsList = new ArrayList<String>();
+
+    public static void setStopWordsList(List<String> stopWordsList) {
+        ReadFromFile.stopWordsList = stopWordsList;
+    }
+
+    public static void setKeyWordsList(List<String> keyWordsList) {
+        ReadFromFile.keyWordsList = keyWordsList;
+    }
+
+    /**
+     * 以字节为单位读取文件。
+     */
+
+    public static void recursiveRead(String path,String resultPath) {
+        File file = new File(path);
+        File[] files = file.listFiles();
+        for(File f : files){
+            if(f.isDirectory()){
+                recursiveRead(f.getPath(),resultPath);
+            }
+            if(f.isFile()){
+                readFileByBytes(f.getPath(),resultPath);
+                //readFileByChars(f.getPath());
+            }
+        }
+    }
+
+    public static void readFileByBytes(String fileName,String resultPath) {
+
+        int docIndex = fileName.lastIndexOf(".");
+        int nameIndex = fileName.lastIndexOf("\\");
+        String resultName = fileName.substring(nameIndex+1,docIndex);
+        String type = fileName.substring(docIndex+1);
+        if(type.equals("java")) {
+            resultName += ".txt";
+
+            //File file = new File(fileName);
+            File os = new File(resultPath + resultName);
+            //File os = new File("data/report_preprocessed/" + resultName);
+            InputStream in = null;
+            OutputStream out;
+            try {
+                System.out.println("以字节为单位读取文件内容，一次读多个字节：");
+                // 一次读多个字节
+
+                int byteread = 0;
+                in = new FileInputStream(fileName);
+                byte[] tempbytes = new byte[in.available()];
+                out = new FileOutputStream(os);
+                ReadFromFile.showAvailableBytes(in);
+
+                // 读入多个字节到字节数组中，byteread为一次读入的字节数
+                while ((byteread = in.read(tempbytes)) != -1) {
+                    //System.out.write(tempbytes, 0, byteread);
+                    String comments = new String(tempbytes);
+
+                    PreProcess.setKeyWordsList(keyWordsList);
+                    PreProcess.setStopWordsList(stopWordsList);
+                    comments = comments.toLowerCase();
+                    comments = PreProcess.removeStopWords(comments);
+                    comments = PreProcess.removeKeyWords(comments);
+
+                    comments = PreProcess.lemmatisation(comments);
+                    comments = PreProcess.splitter(comments);
+                    comments = PreProcess.stemming(comments);
+
+                    out.write(comments.getBytes());
+
+                }
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            } finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e1) {
+                    }
+                }
+            }
+        }
+    }
+    public static void readBugFile(String fileName,String resultPath) {
+
+        int docIndex = fileName.lastIndexOf(".");
+        String resultName = ".txt";
+        String type = fileName.substring(docIndex+1);
+        if(type.equals("xml")) {
+            InputStream in = null;
+            OutputStream out = null;
+            try {
+                System.out.println("以字节为单位读取文件内容，一次读多个字节：");
+                // 一次读多个字节
+
+                int byteread = 0;
+                in = new FileInputStream(fileName);
+                byte[] tempbytes = new byte[in.available()];
+
+                ReadFromFile.showAvailableBytes(in);
+                // 读入多个字节到字节数组中，byteread为一次读入的字节数
+                while ((byteread = in.read(tempbytes)) != -1) {
+                    String comments = new String(tempbytes);
+                    for(int i = 0;i<byteread - 13;i++){
+                        if(comments.substring(i,i+7).equals("bug id=")){
+                            resultName = "report"+comments.substring(i+8,comments.indexOf("\"",i+8))+".txt";
+                            File os = new File(resultPath + resultName);
+                            out = new FileOutputStream(os);
+                        }
+                        if(comments.substring(i,i+13).equals("<description>")){
+                            String comments1 = comments.substring(i+13,comments.indexOf("</description>",i));
+                            PreProcess.setKeyWordsList(keyWordsList);
+                            PreProcess.setStopWordsList(stopWordsList);
+                            comments1 = comments1.toLowerCase();
+                            comments1 = PreProcess.removeStopWords(comments1);
+                            comments1 = PreProcess.removeKeyWords(comments1);
+                            comments1 = PreProcess.lemmatisation(comments1);
+                            comments1 = PreProcess.splitter(comments1);
+                            comments1 = PreProcess.stemming(comments1);
+
+                            out.write(comments1.getBytes());
+
+
+                        }
+                    }
+
+                }
+            }
+            catch (Exception e1) {
+                e1.printStackTrace();
+            }
+            finally {
+                if (in != null) {
+                    try {
+                        in.close();
+                    } catch (IOException e1) {
+                    }
+                }
+            }
+        }
+    }
+    public static void readFileByLines(String fileName, List<String> list) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        try {
+            System.out.println("以行为单位读取文件内容，一次读一整行：");
+            reader = new BufferedReader(new FileReader(file));
+            String tempString = null;
+            int line = 1;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+
+                list.add(tempString);
+                line++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
+        }
+    }
+
+    /**
+     * 显示输入流中还剩的字节数
+     */
+    private static void showAvailableBytes(InputStream in) {
+        try {
+
+            System.out.println("当前字节输入流中的字节数为:" + in.available());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String path1 = "data\\swt-3.1";
+
+        String path2 = "data\\SWTBugRepository.xml";
+        List<String> keyWordsList = new ArrayList<String>();
+        List<String> stopWordsList = new ArrayList<String>();
+        ReadFromFile.readFileByLines("src/main/resources/keyword",keyWordsList);
+        ReadFromFile.readFileByLines("src/main/resources/stopword",stopWordsList);
+        ReadFromFile.setKeyWordsList(keyWordsList);
+        ReadFromFile.setStopWordsList(stopWordsList);
+
+        ReadFromFile.recursiveRead(path1,"data/class_preprocessed2/");
+        ReadFromFile.readBugFile(path2,"data/report_preprocessed2/");
+
+    }
+}
--- src/main/java/preprocess/preprocess.java
+++ src/main/java/preprocess/preprocess.java
@@ -1,13 +1,173 @@
-package preprocess;
+package PreProcess;
 
-public class preprocess {
-    private String text;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.util.CoreMap;
+import org.tartarus.snowball.ext.PorterStemmer;
 
-    public void setText(String text) {
-        this.text = text;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PreProcess {
+
+
+    static List<String> keyWordsList = new ArrayList<String>();
+    static List<String> stopWordsList = new ArrayList<String>();
+
+
+
+
+    public PreProcess(String comments){
+
+        removeStopWords(comments);
+        removeKeyWords(comments);
     }
 
-    /**
-     * 分词，去关键字，去停用词，词性变化，复合拆分
-     */
+    public static void setKeyWordsList(List<String> keyWordsList) {
+        PreProcess.keyWordsList = keyWordsList;
+    }
+
+    public static void setStopWordsList(List<String> stopWordsList) {
+        PreProcess.stopWordsList = stopWordsList;
+    }
+
+    public static String removeKeyWords(String comments){
+        String[] commentsList = comments.split(" ");
+        for(String str1 : keyWordsList){
+            for(int i3 = 0;i3<commentsList.length;i3++){
+                if(str1.equals(commentsList[i3])){
+                    commentsList[i3] = "";
+                }
+            }
+
+        }
+        comments = String.join(" ",commentsList);
+        comments = comments.replaceAll("\\s+"," ");//多空格替换为单空格
+        comments = comments.trim();
+        return comments;
+    }
+    public static String removeStopWords(String comments){
+        int i = comments.indexOf("/*");
+
+        while(i!=-1) {
+            comments = comments.replace(comments.substring(i, comments.indexOf("*/",i)+2), "");
+            i = comments.indexOf("/*");
+        }
+        /*
+        int i0 = comments.indexOf("//");
+
+        while (i0!=-1){
+
+            comments = comments.replace(comments.substring(i0, comments.indexOf("\n",i0)), "");
+            i0 = comments.indexOf("//");
+        }
+
+         */
+        comments = comments.replace("_"," ");
+        comments = comments.replace("\n"," ");
+        for(int i1 = 0;i1<38;i1++){
+            comments = comments.replace(stopWordsList.get(i1)," ");
+        }
+        comments = comments.replaceAll("\\s+"," ");//多空格替换为单空格
+        comments = comments.trim();
+        String[] commentsList = comments.split(" ");
+        for(String str1 : stopWordsList){
+            for(int i3 = 0;i3<commentsList.length;i3++){
+                if(str1.equals(commentsList[i3]) || commentsList[i3].length()==1){
+                    commentsList[i3] = "";
+                }
+            }
+
+        }
+        comments = String.join(" ",commentsList);
+        return comments;
+    }
+
+    public static String lemmatisation(String comments){
+        Properties props = new Properties();  // set up pipeline properties
+        props.put("annotators", "tokenize, ssplit, pos, lemma");   //分词、分句、词性标注和次元信息。
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        String result = "";
+        Annotation document = new Annotation(comments);
+        pipeline.annotate(document);
+        List<CoreMap> words = document.get(CoreAnnotations.SentencesAnnotation.class);
+        for(CoreMap word_temp: words) {
+            for (CoreLabel token: word_temp.get(CoreAnnotations.TokensAnnotation.class)) {
+
+                String lema = token.get(CoreAnnotations.LemmaAnnotation.class);  // 获取对应上面word的词元信息，即我所需要的词形还原后的单词
+                //System.out.println(lema);
+                result+=lema + " ";
+            }
+        }
+        //result = result.substring(0,result.length()-1);
+        return result;
+    }
+
+
+    public static String stemming (String comments) {
+        String[] words = comments.split(" ");
+        PorterStemmer stem = new PorterStemmer();
+        String result = "";
+        for(String word : words) {
+            stem.setCurrent(word);
+            stem.stem();
+            result += stem.getCurrent()+" ";
+        }
+        return result.substring(0,result.length()-1);
+    }
+    public static String splitterLittle(String comments){
+        try {
+            String[] args = new String[]{"python", "D:\\splitter.py", comments};
+            Process proc = Runtime.getRuntime().exec(args);// 执行py文件
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+                comments = line.replace("][", " ");
+                comments = comments.replace("[", "");
+                comments = comments.replace(",", "");
+                comments = comments.replace("]", "");
+                comments = comments.replace("\'", "");
+            }
+            in.close();
+            proc.waitFor();
+            return comments;
+        }catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "";
+
+    }
+    public static String splitter(String comments) {
+
+
+            String result = "";
+            while(comments.length()>20000){
+                String comments1 = comments.substring(0,comments.indexOf(" ",10000));
+                splitterLittle(comments1);
+                result+=comments1;
+                comments = comments.substring(comments.indexOf(" ",10000)+1);
+
+
+            }
+            splitterLittle(comments);
+            result+=comments;
+
+            return result;
+
+
+    }
+
 }
+
+
+
+
+
